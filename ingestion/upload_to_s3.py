e533b887f3cea7ec5edf7916aa3c2663e86726b1
Yurui-Feng <yurifeng@bu.edu>
2024-04-03 12:07:09 -0700

    refactor scripts

--- ingestion/upload_to_s3.py
+++ ingestion/upload_to_s3.py
@@ -1,7 +1,10 @@
 import boto3
 import json
+import os
 
-s3 = boto3.client('s3')
+s3 = boto3.client('s3',
+                  aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 
 bucket_name = 'video-analytics-goalcast'
 
